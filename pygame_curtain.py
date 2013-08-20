a9f58aec48738b5e727c8e6d35b33677f3673b98
Miles Steele <miles@milessteele.com>
2013-08-20 01:46:01 -0400

    fix pygame_curtain

--- pygame_curtain.py
+++ pygame_curtain.py
@@ -1,29 +1,44 @@
+import pygame
+import sys
 from curtain import width, height
 
+
 class Cell(object):
-    def __init__(self, position):
+    def __init__(self, position, size):
         self.position = position
-        self.rect = pygame.Rect((self.position[0]*self.cell_size, self.position[1]*self.cell_size), (self.cell_size, self.cell_size))
-        self.color = 0, 0, 0
+        self.size = size
+        self.rect = pygame.Rect((
+            self.position[0] * self.size,
+            self.position[1] * self.size
+            ),(
+            self.size,
+            self.size
+        ))
+        self.color = 150, 0, 0
+
 
 class PygameCurtain(object):
     def __init__(self):
-        self.width = width
-        self.height = height
-        self.cell_size = 20
-        self.psize = self.pwidth, self.pheight = self.width*self.cell_size, self.height*self.cell_size #in pixels
+        # measurements in cells
+        self.size_cells = width, height
 
-        self.screen = pygame.display.set_mode(self.psize)
+        # measurements in pixels
+        self.cell_scale = 60
+        self.size_px = self.size_cells[0] * self.cell_scale, self.size_cells[1] * self.cell_scale
 
-        #Initialize cells
-        self.cells = []
-        for top in range(0, self.pheight/self.cell_size):
-            cellrow = []
-            for left in range(0, self.pwidth/self.cell_size):
-                cell = Cell((left, top))
-                # cell.alive = bool(random.randrange(100)>75) #randomize livelihood
-                cellrow.append(cell)
-            self.cells.append(cellrow)
+        self.screen = pygame.display.set_mode(self.size_px)
+
+        # initialize grid of cells
+        self.cells = [[Cell((x,y), self.cell_scale) for y in xrange(self.size_cells[1])] for x in xrange(self.size_cells[0])]
+
+        self._render_cells()
+
+    def _render_cells(self):
+        for column in self.cells:
+            for cell in column:
+                cell.rect
+                cell.color
+                pygame.draw.rect(self.screen, cell.color, cell.rect)
 
     def send_color_dict(self, color_dict):
         """
@@ -33,4 +48,20 @@ class PygameCurtain(object):
         in the bounds of `width` and `height` and the values
         are tuples of r, g, b.
         """
-        raise NotImplementedError()
+        # for (x, y), (r, g, b) in color_dict:
+        #     print x, y, r, g, b
+        # for foo in color_dict.items():
+        #     print foo
+        #     print type(foo)
+        for (x, y), (r, g, b) in color_dict.items():
+            self.cells[x][y].color = r * 255, g * 255, b * 255
+
+        self._render_cells()
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    sys.exit()
