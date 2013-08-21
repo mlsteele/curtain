5bcb3d8254948865cb301905a4a750da14cbb6f3
Miles Steele <miles@milessteele.com>
2013-08-20 23:00:08 -0400

    move runner2 to midi_runner, add simple_runner

---
+++ simple_runner.py
@@ -0,0 +1,33 @@
+"""
+A simple runner script.
+This should stay simple.
+If you want more features, copy the file.
+"""
+
+from viewmanagers import SlideShow, ViewManager
+from curtain import Curtain
+from plugins.strobe import Strobe
+from plugins.rainbow import Rainbow
+from plugins.snakes import Snakes
+from plugins.snakes2 import Snakes as Snakes2
+from plugins.ec import EC
+from plugins.fancyrainbow import FancyRainbow
+from plugins.sometext import SomeText
+from plugins.sidescroll import SideScroll
+from plugins.wave import Wave
+from plugins.heightlines import HeightLines
+
+
+bg = SlideShow(15)
+bg.add(EC)
+bg.add(Wave)
+bg.add(Snakes2)
+bg.add(FancyRainbow)
+bg.add(Strobe)
+bg.add(Snakes)
+bg.add(SideScroll)
+bg.add(HeightLines)
+
+curtain = Curtain()
+vm = ViewManager(curtain=curtain, bg=bg)
+vm.start()
